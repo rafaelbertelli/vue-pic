7c50a607f42214e228c6ed0da5b5031c005d15a5
Rafael Borges <rafael.borges@luizalabs.com>
2021-07-13 21:15:49 -0300

    create component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,72 @@
-<template>
-  <div>
-    <h1>
-      {{ titulo }}
-    </h1>
 
-    <picture v-for="foto of fotos">
-      <img :src="foto.url" :alt="foto.titulo" :title="foto.titulo" />
-    </picture>
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+  components: {
+    'meu-painel' : Painel
+  },
+
   data() {
+
     return {
-      titulo: "Meu vue-pic",
-      fotos: [],
-    };
+
+      titulo: 'Alurapic', 
+      fotos: []
+    }
   },
 
   created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then((res) => (this.fotos = res.body))
-      .catch((err) => console.log(err));
-  },
-};
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+
 </script>
 
-<style lang="scss"></style>
+<style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+
+    width: 100%;
+  }
+</style>

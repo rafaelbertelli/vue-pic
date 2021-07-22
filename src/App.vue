7b6366d80883b526d5ee4f3e30d99f45d198c018
Rafael Borges <rafael.borges@luizalabs.com>
2021-07-21 23:58:58 -0300

    vue router setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="corpo">
-    <!-- rotas -->
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/cadastro/Cadastro.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2 class="centralizado">Centralizado</h2>
+</template>
+
+<script></script>
+
+<style>
+.centralizado {
+}
+</style>

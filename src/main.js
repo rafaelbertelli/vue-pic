9f963fd16e2eafe98adbf6221bdd68ebea5c43ca
Rafael Borges <rafael.borges@luizalabs.com>
2021-07-21 23:51:09 -0300

    install vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueResource from 'vue-resource'
+import Vue from "vue";
+import App from "./App.vue";
+import VueResource from "vue-resource";
+import VueRouter from "vue-router";
 
-Vue.use(VueResource)
+Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});

24ae1a7710a8b93250e3925e80aed7a4431d8921
Rafael Borges <rafael.borges@luizalabs.com>
2021-07-22 00:26:34 -0300

    create menu component

---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="rota in rotas">
+        <router-link :to="rota.path ? rota.path : '/'">{{ rota.titulo }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    rotas: { required: true, type: Array },
+  },
+};
+</script>
+
+<style scoped>
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
+}
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+</style>

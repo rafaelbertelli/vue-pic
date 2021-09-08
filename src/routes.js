5623144c8d950cdb314df6693ba178c6d573e7bc
Rafael Borges <rafael.borges@luizalabs.com>
2021-09-08 16:27:59 -0300

    use chunck bundles

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import Cadastro from "./components/cadastro/Cadastro.vue";
+const Cadastro = () => System.import("./components/cadastro/Cadastro.vue"); // lazy loading de componente -> faz com que o bundle seja dividido em chuncks, carregando o componente sob demanda
 import Home from "./components/home/Home.vue";
 
 export const routes = [
